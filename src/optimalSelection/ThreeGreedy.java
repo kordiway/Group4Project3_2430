49d310fa842c446262ca524f0bf944e68c3c39a1
Marcos Elgueta <marcoselgueta@outlook.com>
2026-03-26 19:58:14 -0600

    Initialize ThreeGreedy class with variables and methods

--- src/optimalSelection/ThreeGreedy.java
+++ src/optimalSelection/ThreeGreedy.java
@@ -1,5 +1,26 @@
 package optimalSelection;
 
+import java.util.ArrayList;
+
+// Team Name: Group 4
+// Team Members: Korbin Ordiway, Ian Shoell, Kathleen Monahan, Marcos Elgueta
+// Course and Section: CS 2430-002
+// Project Name: Programming Project 3: Optimal selection_PLO-CS-4
+
 public class ThreeGreedy {
+
+	final int MAX_WEIGHT = 700;
+
+	public SubsetResult highestRating() {
+
+	}
+
+	public SubsetResult lightestWeight() {
+
+	}
+
+	public SubsetResult weightToRatingRatio() {
+
+	}
 
 }
